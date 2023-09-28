1f13d7a9f2c696061942746ea277e49287246d7f
Khushi Jain <129265117+KhushiJain2004@users.noreply.github.com>
2023-09-28 10:57:32 +0530

    Final.py
    layout created

---
+++ Final.py
@@ -0,0 +1,27 @@
+
+import tkinter as tk
+from tkinter import ttk
+
+
+
+root = tk.Tk()
+root.title("Screenshot Application")
+root.geometry("+300+100")
+
+# Create a Notebook widget
+notebook = ttk.Notebook(root)
+
+# Create tabs
+tab_about = ttk.Frame(notebook,style="TFrame")
+tab_single_screenshot = ttk.Frame(notebook,style="TFrame")
+tab_multiple_screenshot = ttk.Frame(notebook,style="TFrame")
+
+# Add tabs to the Notebook
+notebook.add(tab_about, text="About")
+notebook.add(tab_single_screenshot, text="Single Screenshot")
+notebook.add(tab_multiple_screenshot, text="Multiple Screenshots")
+
+# Place the Notebook widget
+notebook.pack(fill=tk.BOTH, expand=True)
+
+root.mainloop()
